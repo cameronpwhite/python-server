4286d22b977f38641d88397f4a8de9bc793bceca
Cameron White <cameronpwhite98@gmail.com>
2021-10-15 14:49:30 -0500

    added sql capability to customers/employees/animals/locations

--- customers/request.py
+++ customers/request.py
@@ -1,3 +1,9 @@
+
+from models.customer import Customer
+import sqlite3
+import json
+
+
 CUSTOMERS = [
     {
         "id": 15,
@@ -28,18 +34,56 @@ CUSTOMERS = [
 
 
 def get_all_customers():
-    return CUSTOMERS
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.address,
+            a.email,
+            a.password
+        FROM customer a
+        """)
+
+        customers = []
+
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+
+            customer = Customer(row['id'], row['name'], row['address'],
+                                row['email'], row['password'])
+
+            customers.append(customer.__dict__)
+
+    return json.dumps(customers)
 
 
 def get_single_customer(id):
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    requested_customer = None
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.address,
+            a.email,
+            a.password
+        FROM customer a
+        WHERE a.id = ?
+        """, ( id, ))
 
-    for customer in CUSTOMERS:
-        if customer["id"] == id:
-            requested_customer = customer
+        data = db_cursor.fetchone()
 
-    return requested_customer
+        customer = Customer(data['id'], data['name'], data['address'],
+                            data['email'], data['password'])
+        
+    return json.dumps(customer.__dict__)
 
 
 def create_customer(customer):
@@ -56,18 +100,18 @@ def create_customer(customer):
 
 def delete_customer(id):
 
-  customer_index = -1
+    customer_index = -1
 
-  for index, customer in enumerate(CUSTOMERS):
-    if customer["id"] == id:
-      customer_index = index
+    for index, customer in enumerate(CUSTOMERS):
+        if customer["id"] == id:
+            customer_index = index
 
-  if customer_index >= 0:
-    CUSTOMERS.pop(customer_index)
+    if customer_index >= 0:
+        CUSTOMERS.pop(customer_index)
 
 def update_customer(id, new_customer):
 
-  for index, customer in enumerate(CUSTOMERS):
-    if customer["id"] == id:
-      CUSTOMERS[index] = new_customer
-      break
+    for index, customer in enumerate(CUSTOMERS):
+        if customer["id"] == id:
+            CUSTOMERS[index] = new_customer
+            break
--- locations/request.py
+++ locations/request.py
@@ -1,3 +1,8 @@
+import sqlite3
+import json
+from models.location import Location
+
+
 LOCATIONS = [
       {
         "id": 1,
@@ -10,29 +15,60 @@ LOCATIONS = [
     ]
 
 def get_all_locations():
-    return LOCATIONS
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+          a.id,
+          a.name,
+          a.address
+        FROM location a
+        """)
+
+        locations = []
+
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            location = Location(row['id'], row['name'], row['address'])
+
+            locations.append(location.__dict__)
+
+    return json.dumps(locations)
 
 def get_single_location(id):
-    
-    requested_location = None
+    with sqlite3.connect("./kennel.db") as conn:
+      conn.row_factory = sqlite3.Row
+      db_cursor = conn.cursor()
 
-    for location in LOCATIONS:
-        if location["id"] == id:
-            requested_location = location
+      db_cursor.execute("""
+      SELECT
+        a.id,
+        a.name,
+        a.address
+      FROM location a
+      WHERE a.id = ?
+      """, ( id, ))
 
-    return requested_location
+      data = db_cursor.fetchone()
+
+      location = Location(data['id'], data['name'], data['address'])
+
+    return json.dumps(location.__dict__)
 
 def create_location(location):
 
-  max_id = LOCATIONS[-1]["id"]
+    max_id = LOCATIONS[-1]["id"]
 
-  new_id = max_id + 1
+    new_id = max_id + 1
 
-  location["id"] = new_id
+    location["id"] = new_id
 
-  LOCATIONS.append(location)
+    LOCATIONS.append(location)
 
-  return location
+    return location
 
 def delete_location(id):
 
@@ -42,12 +78,12 @@ def delete_location(id):
     if location["id"] == id:
       location_index = index
 
-  if location_index >= 0:
-    LOCATIONS.pop(location_index)
+    if location_index >= 0:
+      LOCATIONS.pop(location_index)
 
 def update_location(id, new_location):
 
-  for index, location in enumerate(LOCATIONS):
-    if location["id"] == id:
-      LOCATIONS[index] = new_location
-      break
+    for index, location in enumerate(LOCATIONS):
+        if location["id"] == id:
+            LOCATIONS[index] = new_location
+            break
